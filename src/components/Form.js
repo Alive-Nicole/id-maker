c06481852df5570efa4c54cef619f2549bf2cd54
Mirandamm228 <codingwith.miranda@gmail.com>
2018-08-10 10:21:05 -0700

    added some styling and adjusting birthday input to be more user friendly

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './Card';
+import '../styling/Form.css';
 
 class Form extends React.Component {
     constructor(){
@@ -16,26 +17,29 @@ class Form extends React.Component {
     render(){
         return(
             <div>
-                <label for="firstName">First Name: </label>
-                <br/>
-                <input id="firstName" placeholder="First Name"
-                    onChange={event => this.setState({fname: event.target.value})}/>
-                <br />
-                <label for="lastName">Last Name: </label>
-                <br/>
-                <input id="lastName" placeholder="Last Name"
-                    onChange={event => this.setState({lname: event.target.value})}/>
-                <br/>
-                <label for="birthday">Birthday: </label>
-                <br/>
-                <input id="birthday" placeholder="Birthday"
-                    onChange={event => this.setState({bday: event.target.value})}/>
-                <br />
-                <label for="phoneNumber">Phone Number: </label>
-                <br/>
-                <input id="phoneNumber" placeholder="Phone Number"
-                    onChange={event => this.setState({number: event.target.value})}/>
-                <br/>
+                <form id="form-container">
+                    <label for="firstName">First Name: </label>
+                    <br/>
+                    <input id="firstName" 
+                        onChange={event => this.setState({fname: event.target.value})}/>
+                    <br />
+                    <label for="lastName">Last Name: </label>
+                    <br/>
+                    <input id="lastName" 
+                        onChange={event => this.setState({lname: event.target.value})}/>
+                    <br/>
+                    <label for="birthday">Birthday: </label>
+                    <br/>
+                    <input id="birthday"  type="date"
+                        onChange={event => this.setState({bday: event.target.value})}/>
+                    <br />
+                    <label for="phoneNumber">Phone Number: </label>
+                    <br/>
+                    <input id="phoneNumber"
+                        onChange={event => this.setState({number: event.target.value})}/>
+                    <br/>
+                </form>
+                
                 <hr />
                 <Card fname={this.state.fname} lname={this.state.lname} bday={this.state.bday} number={this.state.number}/>
             </div>
